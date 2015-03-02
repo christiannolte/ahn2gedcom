d8b30531c3c663a25ed4ad567bd8f9e70f3c0247
Christian Nolte <christian.nolte@nomadrail.com>
2015-03-02 12:14:53 +0100

    Programmablauf verbessert

--- filereader.py
+++ filereader.py
@@ -129,7 +129,7 @@ while i<entries:
 #Personen mit unbekannten Geschlecht finden
 for i in myind._individuals:
     if i._geschlecht!="männlich" and i._geschlecht!="weiblich":
-        print "--------------------------------------------"
+        print "-----------------------------------------------------------------------------------"
         print i._vorname+"/"+i._name+ " wurde mit unbekannten Geschlecht gefunden"
         newgender=raw_input ("Geschlecht m(aennlich)/w(eiblich)/u(nbekannt) m/w/u? ")
         if newgender=="m":
@@ -141,6 +141,7 @@ for i in myind._individuals:
 #Personen mit Schraegstrich im Namen finden
 for i in myind._individuals:
     if i._vorname.find("/")!=-1:
+        print "-----------------------------------------------------------------------------------"
         print "Schraegstrich in Vorname gefunden, Ein Vorname darf keinen Schraegstrich enthalten"
         print i._vorname
         i._vorname=raw_input ("Geben Sie dan Namen ohne Schraegstrich ein! ")
